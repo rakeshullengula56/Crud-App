0f8caac13a44104c67f259cb2edf0749fc77812a
rakeshullengula56 <ramullengularakesh@gmail.com>
2025-03-11 00:12:15 +0530

    created Crud App using node.js express.js mongodb and ejs

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+mongoose.connect(`mongodb://127.0.0.1:27017/cruddb`);
+
+const userSchema=mongoose.Schema({
+    name:String,
+    email:String,
+    imageurl:String
+});
+
+module.exports=mongoose.model('user',userSchema);
